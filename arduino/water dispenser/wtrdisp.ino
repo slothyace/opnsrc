ffd0e25949e94e7907edabd3d434a1f4b68ffc60
Acedia <110048632+sinfuls-acedia@users.noreply.github.com>
2024-02-21 14:26:04 +0800

    Create wtrdisp.ino

---
+++ arduino/water dispenser/wtrdisp.ino
@@ -0,0 +1,48 @@
+#include <Servo.h> //sets code to use the Servo.h library
+
+//creates servo obj
+Servo myservo1;
+Servo myservo2;
+
+//define constants
+const int poscls1 = <clsangle>;
+const int posopn1 = <opnangle>;
+const int poscls2 = <clsangle>;
+const int posopn2 = <opnangle>;
+const int trigPin = <pin>;
+const int echoPin = <pin>;
+
+//define vars
+long duration;
+int distance;
+
+void setup() {
+  myservo1.attach(<pin>); //associates pin to data channel for servo 1 (Gate Valve)
+  myservo2.attach(<pin>); //associates pin to data channel for servo 2 (Spring Valve)
+  pinMode(trigPin, OUTPUT); //sets trigger pin to output
+  pinMode(echoPin, INPUT); //sets echo pin to input
+}
+
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW); //clears out the ultrasonic sensor
+  
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.034 / 2; //calculation of distance
+  
+  Serial.print("Distance: ");
+  Serial.println(distance); //debug layer
+
+  if(distance <=10){
+    myservo2.write(posopn2); //closes spring valve
+    delay(1000);
+    myservo1.write(posopn1); //open gate valve
+  }
+  else if(distance >10){
+    myservo1.write(poscls1); //close gate valve
+    myservo2.write(poscls2); //open spring valve
+    }
+}
